2c1033af6caa04b33677a782e8c03e36baab5770
andrieee44 <andrieee44@gmail.com>
2026-01-20 15:26:32 +0800

    lexer: Add doc.go

---
+++ lexer/doc.go
@@ -0,0 +1,5 @@
+// Package lexer provides primitives for building lexical analyzers over
+// arbitrary input streams. It exposes a Reader type that manages buffered
+// input, position tracking, and token emission, along with helper methods
+// for consuming runes according to user-defined rules.
+package lexer // import "github.com/andrieee44/langengine/lexer"
